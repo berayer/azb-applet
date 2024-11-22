c1247b141bbcddd022e5bff1388e8a27bb8c0c3c
zbx <2694735157@qq.com>
2024-11-22 14:53:36 +0800

    docs(README): 更新项目介绍和测试地址
    - 修改 README.md，增加项目介绍和测试地址信息
    - 移除 vite-env.d.ts 中未使用的 VITE_WEBSOCKET_SERVER 常量

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -3,8 +3,6 @@
 interface ImportMetaEnv {
   /** 后台api接口地址 */
   readonly VITE_API_SERVER: string
-  /** websocket 服务地址 */
-  readonly VITE_WEBSOCKET_SERVER: string
   /** 当前应用的版本 */
   readonly VITE_APP_VERSION: string
 }
